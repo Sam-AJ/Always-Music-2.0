28b504c60727d1f1944162699b5222577286b950
Samuel Jara <spjara.aravena@gmail.com>
2022-04-12 15:00:42 -0400

    Version de Entrega

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const yargs = require('yargs');
 const mensajesErrores = require('./errores.js');
 
 const config = {
@@ -11,7 +12,7 @@ const config = {
 
 const pool = new Pool(config);
 
-yargs.command("registrar", "Insertar registro de estudiante",{
+yargs.command("registrar", "Insertar registro de estudiante",{ 
     rut:{
         describe: 'Identificación única del estudiante',
         demand: true,
@@ -35,7 +36,7 @@ yargs.command("registrar", "Insertar registro de estudiante",{
 }, async (argumentos) => {
     let { rut, nombre, curso, nivel } = argumentos;
     const config = {
-        text: "INSERT INTO estudiantes(rut, nombre, curso, nivel) VALUES($1, $2, $3, $4) RETURNING *",
+        text: "INSERT INTO estudiantes(rut, nombre, curso, nivel) VALUES($1, $2, $3, $4) RETURNING *", //Agregar un nuevo estudiante.
         values: [rut, nombre, curso, nivel],
         rowMode: 'array'
     }
@@ -48,20 +49,24 @@ yargs.command("registrar", "Insertar registro de estudiante",{
 }).command("consultar","Obtener lista de todos los estudiantes y estudiante por rut", {
     rut:{
         describe: 'Identificación única del estudiante',
-        demand: true,
+        demand: false,
         alias: 'r'
     }
 }, async (argumentos) => {
     try {
         if(argumentos.rut){
             const config = {
-                text: "SELECT * FROM estudiantes WHERE id=$1",
+                text: "SELECT * FROM estudiantes WHERE rut=$1", //Consultar estudiante por rut.
                 values: [argumentos.rut]
             };
             let res = await pool.query(config);
-            console.table(res.rows);
+            if (res.rows.length == 0){
+                console.log("Rut incorrecto, por favor verifique la lista");
+            }else{
+                console.table(res.rows);
+            }
         }else{
-            let sql = "SELECT * FROM estudiantes";
+            let sql = "SELECT * FROM estudiantes"; //Consultar los estudiantes registrados.
             let res = await pool.query(sql);
             console.table(res.rows);
         }
@@ -92,12 +97,16 @@ yargs.command("registrar", "Insertar registro de estudiante",{
 }, async(argumentos) =>{
     let { rut, nombre, curso, nivel } = argumentos;
     const config = {
-        text:"UPDATE estudiantes SET nombre=$1, curso=$2, nivel=$3 WHERE id=$4 RETURNING *",
+        text:"UPDATE estudiantes SET nombre=$1, curso=$2, nivel=$3 WHERE rut=$4 RETURNING *", //Actualizar la información de un estudiante.
         values:[nombre, curso, nivel, rut]
     }
     try {
         const res = await pool.query(config);
-        console.table(res.rows);
+        if (res.rows.length == 0){
+            console.log("Rut incorrecto, por favor verifique la lista");
+        }else{
+            console.table(res.rows);
+        }
     } catch (error) {
         console.log(mensajesErrores(error.code));
     }
@@ -110,13 +119,13 @@ yargs.command("registrar", "Insertar registro de estudiante",{
 }, async (argumentos) => {
     let rut = argumentos.rut;
     const config = {
-        text: "DELETE FROM estudiantes WHERE rut= $1 RETURNING *",
+        text: "DELETE FROM estudiantes WHERE rut= $1 RETURNING *", //Eliminar el registro de un estudiante.
         values: [rut]
     }
     try {
         const res = await pool.query(config);
         if (res.rows.length == 0){
-            console.log("El estudiante no exite, por favor verifique la lista");
+            console.log("El estudiante no existe, por favor verifique la lista");
         }else{
             console.table(res.rows);
         }
